3d1e02b8ed671342286ed9d26f6f7cdf1fbd2fb5
Sergy <gorokh.sergy@gmail.com>
2022-05-22 16:00:45 +0300

    firstCommit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img
+          src='https://nissi-beach.com/wp-content/uploads/2016/11/NissiBeach-21-1620x600.jpg'
+          alt='alt'
+        />
+      </div>
+      <div>ava + description</div>
+      <div>my posts</div>
+      <div>new post</div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
