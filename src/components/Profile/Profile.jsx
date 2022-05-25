8b9afbe8419baa7123da800eb5afb0267aacb21f
Sergy <gorokh.sergy@gmail.com>
2022-05-25 13:43:34 +0300

    contet wrapper maded

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import styles from './Profile.module.css'
 
 const Profile = () => {
   return (
-    <div className={styles.content}>
+    <div>
       <div>
         <img
           src='https://nissi-beach.com/wp-content/uploads/2016/11/NissiBeach-21-1620x600.jpg'
