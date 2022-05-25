4e43b02175d9dabf76e3c748b8a2c795f5ea71c4
Sergy <gorokh.sergy@gmail.com>
2022-05-25 13:41:21 +0300

    avatar and like added to post jsx

--- src/components/Profile/MyPosts/post/Post.jsx
+++ src/components/Profile/MyPosts/post/Post.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 import styles from './Post.module.css'
 
 const Post = (props) => {
-  return <div className={styles.item}>{props.message}</div>
+  return (
+    <div className={styles.item}>
+      <img
+        src='http://ja-dyadya.narod.ru/FLV/3-BOGATYRJA/bogatyri.gif'
+        alt='alt'
+      />
+      {props.message}
+      <div>
+        <span>like</span> {props.likesCount}
+      </div>
+    </div>
+  )
 }
 
 export default Post
