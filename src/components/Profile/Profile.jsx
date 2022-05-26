07ca73139ab18216ee2787c0a7cdecae494a12f6
Sergy <gorokh.sergy@gmail.com>
2022-05-26 15:00:42 +0300

    some styles changes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,6 +7,7 @@ const Profile = () => {
     <div>
       <div>
         <img
+          className={styles.imgg}
           src='https://nissi-beach.com/wp-content/uploads/2016/11/NissiBeach-21-1620x600.jpg'
           alt='alt'
         />
